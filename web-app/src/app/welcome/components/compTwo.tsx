15d6c1764c1e988b211b01fb9565664ee1bae422
mikeheiser9 <mikeheiser9@gmail.com>
2023-10-03 11:08:46 -0400

    saving timeline changes

--- web-app/src/app/welcome/components/compTwo.tsx
+++ web-app/src/app/welcome/components/compTwo.tsx
@@ -5,11 +5,23 @@ import 'react-vertical-timeline-component/style.min.css';
 export default function CompTwo() {
   return (
     <div className='relative w-screen h-full flex flex-col justify-center items-center'>
+    <div className='w-full text-center'>
+      <h2 className='text-[50px] font-headers'>What We've Built</h2>
+    </div>
     <div className='w-full px-[50px] py-[80px] flex flex-col'>
       <VerticalTimeline
         lineColor='#13222a'
+        animate='true'
+        className='before:!w-[0px] before:!border-dashed before:!border-2'
+
       >
-        <VerticalTimelineElement>
+        <VerticalTimelineElement
+          className='!padding-0 '
+          iconStyle={{display: 'none'}}
+        >
+          <div>
+            <h1>Cunty cunt hole</h1>
+          </div>
 
         </VerticalTimelineElement>
       </VerticalTimeline>
