dd24a12fc9daa74b05fcc9792d59beb563f97f6c
mikeheiser9 <mikeheiser9@gmail.com>
2023-10-05 14:42:38 -0400

    added head to layout

--- web-app/src/app/layout.tsx
+++ web-app/src/app/layout.tsx
@@ -7,6 +7,7 @@ import { axiosParse } from "@/axios/config";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import { Toast } from "@/components/toast";
+import Head from "next/head";
 
 export default function RootLayout({
   children,
@@ -16,10 +17,28 @@ export default function RootLayout({
   axiosParse(store);
   return (
     <html lang="en" className="!p-0 !m-0">
-      <head>
+      <Head>
+        <title>EduRx</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:title" content="EduRx" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/edurxLogo.svg" />
+        <meta property="og:url" content="https://edu-rx.com/" />
+        {/* <meta name="twitter:card" content="summary_large_image" /> */}
+
+        {/* <!--  Non-Essential, But Recommended --> */}
+        <meta property="og:description" content="Welcome to EduRx. Beta launching Q4 2023" />
+        <meta property="og:site_name" content="Welcome to EduRx. Beta launching Q4 2023" />
+        {/* <meta name="twitter:image:alt" content="Welcome to EduRx. Beta launching Q4 2023" /> */}
+
+        {/* <!--  Non-Essential, But Required for Analytics --> */}
+        {/* <meta name="twitter:site" content="@gemsetnft" /> */}
+        <link rel="shortcut icon" href="/edurxLogo.svg" />
+    </Head>
+      {/* <head>
         <title>EduRx</title>
         <link rel="shortcut icon" href="/edurxLogo.svg" />
-      </head>
+      </head> */}
       <body className={'w-full flex-auto flex bg-eduBlack font-body'}>
         <Provider store={store}>
           {/* <Header /> */}
